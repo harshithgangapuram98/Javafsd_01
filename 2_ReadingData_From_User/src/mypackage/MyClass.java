63544ca1d7a980231e028e086d27240a8af65698
harshith <harshithgangapuram98@gmail.com>
2022-07-14 14:58:13 +0530

    reading data from user

---
+++ 2_ReadingData_From_User/src/mypackage/MyClass.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class MyClass 
+{
+	public static void main(String[] args)
+	{
+		Scanner obj = new Scanner(System.in);
+		String name;
+		float salary;
+		int age;
+		
+		System.out.println("Enter your namr:");
+		name = obj.nextLine();
+		
+		System.out.println("Enter your salary:");
+		salary = obj.nextFloat();
+		
+		System.out.println("Enter your age:");
+		age = obj.nextInt();
+		
+		
+		System.out.println("Name = "+name);
+		System.out.println("salary = "+salary);
+		System.out.println("age = "+age);
+	}
+}
