1e9d05ca8455bc8c412a7634f94bc11f228d84fb
harshith <harshithgangapuram98@gmail.com>
2022-07-14 15:55:25 +0530

    LearnStringsInJava

---
+++ LearnStringsInJava/src/mypackage/MyClass.java
@@ -0,0 +1,22 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	//	String name = "harsha";
+	//	String data  = "harsha";
+		
+	//	String data = new String ("harsha");
+		
+		String s1= new String ("meg");
+		String s2= new String("meg");
+		
+		if(s1==s2)
+			System.out.println("Equal");
+		else
+			System.out.println("Not equal");
+		
+
+	}
+
+}
